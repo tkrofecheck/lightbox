33a24a7f7f91ba93ae7fb61358b526abc7981f23
Tim Krofecheck <tkrofecheck@gmail.com>
2017-10-11 09:23:20 -0400

    constructor with api url

---
+++ src/lightbox.js
@@ -0,0 +1,7 @@
+function Lightbox(url) {
+	this.apiUrl = url;
+}
+
+var Gallery = new Lightbox(
+	'https://www.googleapis.com/customsearch/v1?cx=003855216133477760451%3A4zvjz-bh334&cr=true&imgType=photo&q=cars&safe=high&searchType=image&key=AIzaSyAVGFZDgKUNvMMjLi5I4uS0f0ag6ETGHLw'
+);
