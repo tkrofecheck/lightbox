d191dffc262ee67a2d571a44234f5c69bb4b633e
Tim Krofecheck <tkrofecheck@gmail.com>
2017-10-11 10:05:43 -0400

    cleanup code a little

--- src/lightbox.js
+++ src/lightbox.js
@@ -1,28 +1,46 @@
-function getData(obj, url) {
-	var xhr;
+function Lightbox(url) {
+	this.apiUrl = url;
+	this.responseJson = null;
+}
+
+Lightbox.prototype.getData(url) {
+	var _this = this,
+		xhr;
+
 	if (window.XMLHttpRequest) {
 		xhr = new XMLHttpRequest();
 	} else {
 		// code for older browsers
 		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
 	}
+
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			obj.responseJson = JSON.parse(this.responseText);
-			obj.ready();
+			_this.responseJson = JSON.parse(this.responseText);
+			_this.ready();
 		}
 	};
+	
 	xhr.open('GET', url, true);
 	xhr.send();
 }
 
-function Lightbox(url) {
-	this.apiUrl = url;
-	this.responseJson = null;
-}
+Lightbox.prototype.search = function(value) {
+	console.log('select', this);
+};
 
-Lightbox.prototype.init = function() {
-	getData(this, this.apiUrl);
+Lightbox.prototype.render = function(searchQuery) {
+	if (!searchQuery) {
+		console.log('need query to render');
+		return;
+	}
+
+	var apiUrl =
+		'https://www.googleapis.com/customsearch/v1?cx=003855216133477760451%3A4zvjz-bh334&cr=true&imgType=photo&q=' +
+		searchQuery +
+		'&safe=high&searchType=image&key=AIzaSyAVGFZDgKUNvMMjLi5I4uS0f0ag6ETGHLw';
+
+	getData(this, apiUrl);
 };
 
 Lightbox.prototype.ready = function() {
@@ -34,8 +52,3 @@ Lightbox.prototype.createDOM = function() {
 	var gallery = document.getElementById('gallery');
 	gallery.innerHTML = this.responseJson;
 };
-
-var Gallery = new Lightbox(
-	'https://www.googleapis.com/customsearch/v1?cx=003855216133477760451%3A4zvjz-bh334&cr=true&imgType=photo&q=cars&safe=high&searchType=image&key=AIzaSyAVGFZDgKUNvMMjLi5I4uS0f0ag6ETGHLw'
-);
-Gallery.init();
